13f2ce0918afbe7fb4d59aeb04f42fdc913ef7a8
shiraz <shiraz@qburst.com>
2019-06-13 14:42:25 +0530

    project files

--- src/rectangle.java
+++
@@ -1,8 +0,0 @@
-public class Rectangle {
-  public int length;
-  public int width;
-           
-  public Rectangle(int lengt, int width) {
-    this.lenght = length;
-    this.width = width;
-  public int getArea{} {
